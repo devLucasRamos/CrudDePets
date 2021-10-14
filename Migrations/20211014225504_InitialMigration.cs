d5ab09a1167b77edfeb6ce8faecfa6bbb96ab8c4
Lucas Ramos <lucas2adriano@gmail.com>
2021-10-14 20:05:37 -0300

    Adicionando migrations e o petsdb

---
+++ Migrations/20211014225504_InitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrudDePets.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DonoNome = table.Column<string>(type: "TEXT", nullable: true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    TipoDePet = table.Column<string>(type: "TEXT", nullable: true),
+                    Idade = table.Column<int>(type: "INTEGER", nullable: false),
+                    Cor = table.Column<string>(type: "TEXT", nullable: true),
+                    DataDeRegistro = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pets");
+        }
+    }
+}
