8df80a188bfe4f70876a8343886e90629720708d
Lucas Ramos <lucas2adriano@gmail.com>
2021-10-14 21:41:13 -0300

    Implementando na Controllers, com Post e Delete

--- Controllers/PetsController.cs
+++ Controllers/PetsController.cs
@@ -1,6 +1,9 @@
-﻿using Data;
+﻿using ViewModel;
+using Data;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using Models;
+using System;
 
 namespace Controllers
 {
@@ -10,15 +13,67 @@ namespace Controllers
     {
         [HttpGet]
         [Route(template: "Pets")]
-        public IActionResult Get([FromServices]AppDbContext context)
+        public IActionResult Get([FromServices] AppDbContext context)
         {
             var pets = context.Pets.ToList();
             return Ok(pets);
         }
 
+        [HttpGet]
+        [Route(template: "Pets/{Id}")]
+        public IActionResult GetbyId([FromServices] AppDbContext context, [FromRoute] int id)
+        {
+            var pet = context.Pets.FirstOrDefault(x => x.Id == id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+            return Ok(pet);
+        }
 
+        [HttpPost]
+        [Route(template: "Pets")]
+        public IActionResult Post([FromServices] AppDbContext context, [FromBody] CreateViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var pet = new Pets
+                {
+                    DonoNome = model.DonoNome,
+                    Nome = model.Nome,
+                    TipoDePet = model.TipoDePet,
+                    Idade = model.Idade,
+                    Cor = model.Cor
+                };
+                try
+                {
+                    context.Add(pet);
+                    context.SaveChanges();
+                    return Created(uri: $"v1/Pets/{pet.Id}", pet);
+                }
+                catch (Exception)
+                {
+                    return NotFound();
+                }
+            }
+        }
 
-
-
+        [HttpDelete]
+        [Route(template: "Pets/{Id}")]
+        public IActionResult Delete([FromRoute] int id, [FromServices] AppDbContext context)
+        {
+            var pet = context.Pets.FirstOrDefault(x => x.Id == id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+            context.Pets.Remove(pet);
+            context.SaveChanges();
+            return Ok();
+        }
     }
 }
---
+++ ViewModel/CreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViewModel
+{
+    public class CreateViewModel
+    {
+        [Required]
+        public string DonoNome { get; set; }
+        public string Nome { get; set; }
+        public string TipoDePet { get; set; }
+        public int Idade { get; set; }
+        public string Cor { get; set; }
+    }
+}
